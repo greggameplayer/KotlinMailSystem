17bbbeb68174180076b75838a295c7a6c51da963
Jad <jad.daouk@epsi.fr>
2021-12-10 11:32:46 +0100

    start sendMail

--- app/src/main/java/com/greggameplayer/kotlinmailsystem/MainActivity.kt
+++ app/src/main/java/com/greggameplayer/kotlinmailsystem/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.greggameplayer.kotlinmailsystem
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.greggameplayer.kotlinmailsystem.controllers.EmailController
+import com.greggameplayer.kotlinmailsystem.controllers.SendMail
+
 
 class MainActivity : AppCompatActivity() {
     var emailController : EmailController = EmailController()
@@ -11,5 +15,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         emailController.appExecutors = AppExecutors()
         setContentView(R.layout.activity_main)
+
+        val btPass : Button = findViewById(R.id.bt_pass)
+
+        btPass.setOnClickListener{
+            val intent = Intent(this, SendMail::class.java)
+            startActivity(intent)
+        }
+
     }
+
 }
---
+++ app/src/main/java/com/greggameplayer/kotlinmailsystem/controllers/SendMail.kt
@@ -0,0 +1,16 @@
+package com.greggameplayer.kotlinmailsystem.controllers
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.greggameplayer.kotlinmailsystem.AppExecutors
+import com.greggameplayer.kotlinmailsystem.R
+
+class SendMail : AppCompatActivity() {
+    var emailController: EmailController = EmailController()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        emailController.appExecutors = AppExecutors()
+        setContentView(R.layout.send_mail)
+    }
+}
