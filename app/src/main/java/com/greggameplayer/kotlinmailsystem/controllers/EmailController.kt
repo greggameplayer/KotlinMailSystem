c68dd66488fccc42fa225c8ef149f1e1289f978b
Unknown <gregoire.hage@gmail.com>
2021-12-24 01:53:58 +0100

    put sent mail in sent box and add mail retrieving function
    - put sent mail in sent box after sending
    - add mail retrieving function for specific mailbox (INBOX,SENT,DRAFTS,TRASH)

--- app/src/main/java/com/greggameplayer/kotlinmailsystem/controllers/EmailController.kt
+++ app/src/main/java/com/greggameplayer/kotlinmailsystem/controllers/EmailController.kt
@@ -1,41 +1,61 @@
 package com.greggameplayer.kotlinmailsystem.controllers
 
-import android.net.Network
 import com.greggameplayer.kotlinmailsystem.AppExecutors
 import com.greggameplayer.kotlinmailsystem.beans.Credentials
+import com.sun.mail.imap.IMAPSSLStore
+import java.net.URLEncoder
+import java.util.*
+import java.util.concurrent.Executors
 import javax.activation.DataHandler
 import javax.activation.FileDataSource
-import javax.mail.Address
-import javax.mail.Message
-import javax.mail.Transport
+import javax.mail.*
 import javax.mail.internet.InternetAddress
 import javax.mail.internet.MimeBodyPart
 import javax.mail.internet.MimeMessage
 import javax.mail.internet.MimeMultipart
 
+
+enum class Mailboxes(val value: String) {
+    INBOX("INBOX"),
+    SENT("Sent"),
+    DRAFTS("Drafts"),
+    TRASH("Trash")
+}
+
 class EmailController {
     lateinit var appExecutors: AppExecutors
+
+    private val props: Properties = System.getProperties()
+
+    init {
+        props["mail.smtp.host"] = "mx.gregoire.live"
+        props["mail.imaps.host"] = "mx.gregoire.live"
+        props["mail.smtp.socketFactory.port"] = "587"
+        props["mail.smtp.socketFactory.class"] =
+            "com.greggameplayer.kotlinmailsystem.controllers.AlwaysTrustSSLContextFactory"
+        props["mail.imaps.socketFactory.port"] = "993"
+        props["mail.imaps.socketFactory.class"] =
+            "com.greggameplayer.kotlinmailsystem.controllers.AlwaysTrustSSLContextFactory"
+        props["mail.smtp.auth"] = "true"
+        props["mail.imaps.auth"] = "true"
+        props["mail.smtp.port"] = "587"
+        props["mail.imaps.port"] = "993"
+        props["mail.smtp.ssl.trust"] = "*"
+        props["mail.imaps.ssl.trust"] = "*"
+        props["mail.smtp.starttls.enable"] = "true"
+        props["mail.imaps.starttls.enable"] = "true"
+    }
 
     fun sendEmail(email: String, subject: String, content: String, attachment: String = "") {
         println("Sending email to $email with subject $subject and content $content ${if (attachment.isNotEmpty()) " and attachment $attachment" else ""}")
         appExecutors.diskIO().execute {
-            val props = System.getProperties()
-            props["mail.smtp.host"] = "mx.gregoire.live"
-            props["mail.smtp.socketFactory.port"] = "587"
-            props["mail.smtp.socketFactory.class"] = "com.greggameplayer.kotlinmailsystem.controllers.AlwaysTrustSSLContextFactory"
-            props["mail.smtp.auth"] = "true"
-            props["mail.smtp.port"] = "587"
-            props["mail.smtp.ssl.trust"] = "*"
-            props["mail.smtp.debug"] = "true"
-            props["mail.smtp.starttls.enable"] = "true"
-
-            val session = javax.mail.Session.getDefaultInstance(props,
-                object : javax.mail.Authenticator() {
+            val session = Session.getDefaultInstance(props,
+                object : Authenticator() {
                     //Authenticating the password
-                    override fun getPasswordAuthentication(): javax.mail.PasswordAuthentication {
-                        return javax.mail.PasswordAuthentication(Credentials.EMAIL, Credentials.PASSWORD)
+                    override fun getPasswordAuthentication(): PasswordAuthentication {
+                        return PasswordAuthentication(Credentials.EMAIL, Credentials.PASSWORD)
                     }
-        })
+                })
 
             try {
                 val message = MimeMessage(session)
@@ -54,10 +74,24 @@ class EmailController {
                     message.setContent(multipart)
                 }
 
-                Transport.send(message)
+                appExecutors.networkIO().execute {
+                    Transport.send(message)
+                }
+
+                appExecutors.networkIO().execute {
+                    val store = IMAPSSLStore(session, URLName("imaps://${URLEncoder.encode(Credentials.EMAIL, "UTF-8")}:${Credentials.PASSWORD}@mx.gregoire.live:993"))
+                    store.connect()
+                    val folder = store.getFolder("Sent")
+                    folder.open(Folder.READ_WRITE)
+                    folder.appendMessages(arrayOf(message))
+                    message.setFlag(Flags.Flag.RECENT, true)
+                    message.setFlag(Flags.Flag.SEEN, true)
+                    folder.close()
+                    store.close()
+                }
 
                 appExecutors.mainThread().execute {
-                    println("Email sent successfully")
+                    println("Email sent successfully and saved to Sent folder")
                 }
             } catch (e: Exception) {
                 e.printStackTrace()
@@ -65,8 +99,40 @@ class EmailController {
         }
     }
 
-    fun retrieveAllEmails() {
-        println("Retrieving all emails")
+    fun retrieveAllEmails(mailboxes: Mailboxes, callback: (Array<Message>) -> Unit) {
+        var messages: Array<Message>
+        val networkThread = appExecutors.networkIO()
+        val mainThread = appExecutors.mainThread()
+        val diskThread = appExecutors.diskIO()
+
+        diskThread.execute {
+            val session = Session.getDefaultInstance(props,
+                object : Authenticator() {
+                    //Authenticating the password
+                    override fun getPasswordAuthentication(): PasswordAuthentication {
+                        return PasswordAuthentication(Credentials.EMAIL, Credentials.PASSWORD)
+                    }
+                })
+
+            try {
+
+                networkThread.execute {
+                    val store = IMAPSSLStore(session, URLName("imaps://${URLEncoder.encode(Credentials.EMAIL, "UTF-8")}:${Credentials.PASSWORD}@mx.gregoire.live:993"))
+                    store.connect()
+                    val folder = store.getFolder(mailboxes.value)
+                    folder.open(Folder.READ_ONLY)
+                    messages = folder.messages
+                    folder.close()
+                    store.close()
+                    mainThread.execute {
+                        println("Retrieved ${messages.size} emails")
+                    }
+                    callback.invoke(messages)
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
     }
 
 }
--- app/src/main/java/com/greggameplayer/kotlinmailsystem/controllers/SendMail.kt
+++ app/src/main/java/com/greggameplayer/kotlinmailsystem/controllers/SendMail.kt
@@ -1,5 +1,6 @@
 package com.greggameplayer.kotlinmailsystem.controllers
 
+import android.content.Context
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -7,10 +8,12 @@ import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.greggameplayer.kotlinmailsystem.AppExecutors
+import com.greggameplayer.kotlinmailsystem.MainActivity
 import com.greggameplayer.kotlinmailsystem.R
 
 class SendMail : AppCompatActivity() {
     var emailController: EmailController = EmailController()
+    val context = this
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,8 +35,11 @@ class SendMail : AppCompatActivity() {
 
         btSend.setOnClickListener{
             emailController.sendEmail(etTo.text.toString(), etSubject.text.toString(), etContent.text.toString())
+            /*emailController.retrieveAllEmails(Mailboxes.SENT) { messages ->
+                println("Message : ${messages.size}")
+            }*/
             this.finish()
-            Toast.makeText(this, "Le mail à bien été envoyer.", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "Le mail a bien été envoyé.", Toast.LENGTH_LONG).show()
         }
 
 
