5aaced1d969c6c6847c5f8f1561be5ffe45e4cba
greggameplayer <gregoire.hage@gmail.com>
2022-01-03 11:42:55 +0100

    add retrofit dependencies & login frame displayed directly

--- app/src/main/java/com/greggameplayer/kotlinmailsystem/MainActivity.kt
+++ app/src/main/java/com/greggameplayer/kotlinmailsystem/MainActivity.kt
@@ -5,37 +5,27 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.greggameplayer.kotlinmailsystem.controllers.EmailController
-import com.greggameplayer.kotlinmailsystem.controllers.Login
-import com.greggameplayer.kotlinmailsystem.controllers.SendMail
 import com.greggameplayer.kotlinmailsystem.controllers.SignUp
 
 
 class MainActivity : AppCompatActivity() {
-    var emailController : EmailController = EmailController()
+    var emailController: EmailController = EmailController()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         emailController.appExecutors = AppExecutors()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.connexion)
+        val btLogin = findViewById<Button>(R.id.buttonConnexion)
+        val btSignup = findViewById<Button>(R.id.buttonInscription)
 
-        val btPass : Button = findViewById(R.id.bt_pass)
-        val buttonGoConnexion : Button = findViewById(R.id.buttonGoConnexion)
-        val buttonGoInscription : Button = findViewById(R.id.buttonGoInscription)
-
-        btPass.setOnClickListener{
-            val intent = Intent(this, SendMail::class.java)
-            startActivity(intent)
+        btLogin.setOnClickListener {
+            //
         }
 
-        buttonGoConnexion.setOnClickListener{
-            val intent = Intent(this, Login::class.java)
-            startActivity(intent)
-        }
-        buttonGoInscription.setOnClickListener{
+        btSignup.setOnClickListener {
             val intent = Intent(this, SignUp::class.java)
             startActivity(intent)
         }
-
     }
 
 }
