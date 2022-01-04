220aea61ac54114848898224a5a5cbed04f162fb
Unknown <gregoire.hage@gmail.com>
2022-01-04 13:41:07 +0100

    modify RetrofitResponse bean

--- app/src/main/java/com/greggameplayer/kotlinmailsystem/beans/RetrofitResponse.kt
+++ app/src/main/java/com/greggameplayer/kotlinmailsystem/beans/RetrofitResponse.kt
@@ -4,4 +4,7 @@ class RetrofitResponse {
     var success: Boolean? = null
     var message: String? = null
     var error: String? = null
+    var username : String? = null
+    var password : String? = null
+    var name : String? = null
 }
