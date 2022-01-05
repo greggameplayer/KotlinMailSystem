8987f054ebec38f4e9423aabbb73ed814e29fe7d
Jad <jad.daouk@epsi.fr>
2022-01-05 14:43:10 +0100

    Update Credentials.kt

--- app/src/main/java/com/greggameplayer/kotlinmailsystem/beans/Credentials.kt
+++ app/src/main/java/com/greggameplayer/kotlinmailsystem/beans/Credentials.kt
@@ -2,8 +2,8 @@ package com.greggameplayer.kotlinmailsystem.beans
 
 class Credentials {
     companion object {
-        var EMAIL = "kotlin@gregoire.live"
-        var PASSWORD = "5Rw835P;75"
-        var NAME = "Kotlin"
+        var EMAIL = ""
+        var PASSWORD = ""
+        var NAME = ""
     }
 }
